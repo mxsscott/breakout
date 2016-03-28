55cfd6096f93342e619a90a8875db0a88c8fb24b
Mark Scott <github@mscott.cx>
2016-03-28 20:04:53 +0100

    Add game loop scaffolding

--- src/cx/mscott/breakout/GraphicsCanvas.java
+++ src/cx/mscott/breakout/GraphicsCanvas.java
@@ -1,34 +1,87 @@
 package cx.mscott.breakout;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GraphicsCanvas extends JPanel implements ActionListener {
 	private static final long serialVersionUID = -1659288567154587140L;
 
-	private int width;
-	private int height;
+	/** Game loop runs at 20ms */
+	private final static int GAME_LOOP_SPEED = 20;
+	
+	/** Game screen width */
+	private final static int SCREEN_WIDTH = 400;
+
+	/** Game screen height */
+	private final static int SCREEN_HEIGHT = 400;
+	
+	/** Game loop timer */
+	private Timer timer;
+	
+	/** Active keys */
+	private Set<Integer> keys = new HashSet<Integer>();
 	
 	public GraphicsCanvas() {
-		width = 100;
-		height = 200;
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		setBackground(Color.black);
+		
+		// Listen for key presses
+		addKeyListener(new KeyAdapter() {
+		    public void keyPressed(KeyEvent e) {
+		    	keys.add(e.getKeyCode());
+		    }
+		    
+		    public void keyReleased(KeyEvent e) {
+		    	keys.remove(e.getKeyCode());
+		    }
+        });
+		
+		timer = new Timer(GAME_LOOP_SPEED, this);
+		timer.start(); 
 	}
 	
+	/**
+	 * Action handler. Usually a game loop timer firing.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Paint the game screen.
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		// TODO Game graphics go here...
+	}
+	
+	/**
+	 * Ensure we can capture keyboard focus.
+	 */
 	@Override
 	public boolean isFocusable() {
 		return true;
 	}
 
+	/**
+	 * Define size of game window.
+	 */
 	public Dimension getPreferredSize() {
-		return new Dimension(width, height);
+		return new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT);
 	}
 }
--- src/cx/mscott/breakout/Main.java
+++ src/cx/mscott/breakout/Main.java
@@ -1,10 +1,35 @@
 package cx.mscott.breakout;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Main();
 	}
 
+	public Main() {
+        try {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Breakout");
+
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+            SwingUtilities.invokeLater(new Runnable() {
+            	public void run() {
+        	        new MainWindow();
+            	}
+            });
+     
+        } catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+	}
 }
